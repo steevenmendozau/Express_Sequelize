edc8d86470f0609ec73197868e2d654ef7589ee0
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-07-08 19:55:45 -0500

    Creacion de la vista para mostrar la tabla fotos

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 var sequelize = require('./models').sequelize;
 
+var fotosRouter = require('./routes/fotos');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,14 +24,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/fotos', fotosRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
